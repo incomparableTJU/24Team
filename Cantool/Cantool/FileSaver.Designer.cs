8e9aa9bc0bef7a1218a079e18390dfeba7c795cb
Rainbow0625 <347601193@qq.com>
2017-10-08 15:39:46 +0800

    保存文件为CSV等格式

---
+++ Cantool/Cantool/FileSaver.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Cantool
+{
+    partial class FileSaver
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button saveButton;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(234, 183);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(155, 41);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "保存文件";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button1_Click);
+           
+            // FileSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 471);
+            this.Controls.Add(this.saveButton);
+            this.Name = "FileSaver";
+            this.Text = "保存文件";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        
+
+
+
+
+    }
+}
+
---
+++ Cantool/Cantool/FileSaver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantool
+{
+    public partial class FileSaver : Form
+    {
+        String content = "abcdefg"; //需保存的内容
+
+        public FileSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.RestoreDirectory = true;
+            fileDialog.Title = "请选择文件";
+            //设置要选择的文件的类型“标签1|*.jpg|标签2|.png|标签3|.gif”
+            fileDialog.Filter = "CSV文件|*.csv|XML文件|*.xml|JSON文件|*.json";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //获得文件路径
+                String filename = fileDialog.FileName.ToString();
+                if (filename.Contains("csv"))
+                    content = convertToCSV(content);
+                else if (filename.Contains("xml"))
+                    content = convertToXML(content);
+                else if (filename.Contains("json"))
+                    content = convertToJSON(content);
+                FileInfo fi = new FileInfo(filename);
+                if (!fi.Directory.Exists)
+                {
+                    fi.Directory.Create();
+                }
+                //文件写入
+                FileStream fs = new FileStream(filename, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                //StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.Default);
+                StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+                sw.WriteLine(content);
+                sw.Close();
+                fs.Close();
+
+            }
+        }
+
+        //将文件内容转换成指定文件类型的格式
+        String convertToCSV(String content)
+        {
+            return content;
+        }
+        String convertToXML(String content)
+        {
+            return content;
+        }
+        String convertToJSON(String content)
+        {
+            return content;
+        }
+
+    }
+}
