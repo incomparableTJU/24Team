8448844fdae8131ce9c6a470057003d17da5e3bf
wangqiang <838660384@qq.com>
2017-10-18 20:34:54 +0800

    real curve paint

---
+++ curvepaint/curvepaint/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace curvepaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();      
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+
+            // btnStart
+
+            // 
+
+            this.btnStart.Location = new System.Drawing.Point(621, 10);
+
+            this.btnStart.Name = "btnStart";
+
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+
+            this.btnStart.TabIndex = 0;
+
+            this.btnStart.Text = "开始";
+
+            this.btnStart.UseVisualStyleBackColor = true;
+
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            // 
+
+            // chart1
+
+            // 
+
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+
+            this.chart1.Location = new System.Drawing.Point(12, 42);
+
+            this.chart1.Name = "chart1";
+
+            this.chart1.Size = new System.Drawing.Size(884, 422);
+
+            this.chart1.TabIndex = 1;
+
+            this.chart1.Text = "chart1";
+           
+           //this.chart1.AxisScrollBarClicked = System.Windows.Forms.ScrollBars.Horizontal;
+
+
+            // 
+
+            // timer1
+
+            // 
+
+            this.timer1.Interval = 300;
+
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+            // 
+
+            // btnStop
+
+            // 
+
+            this.btnStop.Location = new System.Drawing.Point(721, 10);
+
+            this.btnStop.Name = "btnStop";
+
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+
+            this.btnStop.TabIndex = 2;
+
+            this.btnStop.Text = "停止";
+
+            this.btnStop.UseVisualStyleBackColor = true;
+
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+     
+            //Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+
+            this.ClientSize = new System.Drawing.Size(908, 476);
+                     
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "curve";
+            this.Text = "curvegraph";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+
+        }
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;   
+     
+
+    }
+}
---
+++ curvepaint/curvepaint/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace curvepaint
+{
+    public partial class Form1 : Form
+    {
+        private Queue<double> dataQueue = new Queue<double>(100);
+        private int curValue = 0;
+        private int num = 10;//每次删除增加几个点
+        public  Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+
+        /// 开始事件
+
+        /// </summary>
+
+        /// <param name="sender"></param>
+
+        /// <param name="e"></param>
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+            InitChart();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+
+        {
+            this.timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+
+        {
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+            }
+        }
+
+        private void InitChart()
+
+        {
+
+            //定义图表区域
+
+            this.chart1.ChartAreas.Clear();
+
+            ChartArea chartArea1 = new ChartArea("C1");
+
+            this.chart1.ChartAreas.Add(chartArea1);
+
+            //定义存储和显示点的容器
+
+            this.chart1.Series.Clear();
+
+            Series series1 = new Series("S1");
+
+            series1.ChartArea = "C1";
+
+            this.chart1.Series.Add(series1);
+
+            //设置图表显示样式
+
+            this.chart1.ChartAreas[0].AxisY.Minimum = 0;
+
+            this.chart1.ChartAreas[0].AxisY.Maximum = 100;
+
+            this.chart1.ChartAreas[0].AxisX.Interval = 5;
+
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+        //    chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+         //   chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = false;
+         //   chart1.ChartAreas[0].AxisX.ScaleView.Size = 20;
+
+            //设置标题        
+            this.chart1.Titles.Add("S01");
+
+            this.chart1.Titles[0].Text = "CanTool数据的物理值显示";
+
+            this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
+
+            this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+
+            //设置图表显示样式
+
+            this.chart1.Series[0].Color = Color.Red;
+            //线的类型
+            this.chart1.Series[0].ChartType = SeriesChartType.Spline;       
+            this.chart1.Series[0].Points.Clear();
+
+        }
+        //更新队列中的值
+        private void UpdateQueueValue()
+        {
+            if (dataQueue.Count > 100)
+            {
+                //先出列
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Dequeue();
+                }
+            }
+            for (int i = 0; i < num; i++)
+            {
+
+                //对curValue只取[0,360]之间的值
+                
+                //curValue = curValue % 360;
+                //dataQueue.Enqueue((50 * Math.Sin(curValue * Math.PI / 180)) + 50);  //对得到的正玄值，放大50倍，并上移50
+                //     curValue = curValue + 10;
+                Random ro = new Random();
+                curValue = ro.Next(0, 100);           
+                dataQueue.Enqueue(curValue);
+               }          
+        }
+
+    }
+}
