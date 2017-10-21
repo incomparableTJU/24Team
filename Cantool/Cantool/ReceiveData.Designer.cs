d1fd95e62afec5121ae6c4159d36e04b28425067
miaoxu9999 <2229009552@qq.com>
2017-10-21 14:50:24 +0800

    显示数据的窗体添加

---
+++ Cantool/Cantool/ReceiveData.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Set_Com
+{
+    partial class fm_receivedata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx_receivedata = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbx_receivedata
+            // 
+            this.tbx_receivedata.Location = new System.Drawing.Point(20, 28);
+            this.tbx_receivedata.Multiline = true;
+            this.tbx_receivedata.Name = "tbx_receivedata";
+            this.tbx_receivedata.Size = new System.Drawing.Size(381, 256);
+            this.tbx_receivedata.TabIndex = 0;
+            // 
+            // fm_receivedata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 352);
+            this.Controls.Add(this.tbx_receivedata);
+            this.Name = "fm_receivedata";
+            this.Text = "接收数据";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_receivedata;
+    }
+}
---
+++ Cantool/Cantool/ReceiveData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Set_Com
+{
+    public partial class fm_receivedata : Form
+    {
+        /// <summary>
+        /// Com口是否设定
+        /// </summary>
+        private bool set_com = false;
+        /// <summary>
+        /// 创建私有串口资源
+        /// </summary>
+        private SerialPort comDevice ;
+        public fm_receivedata()
+        {
+            InitializeComponent();
+            MessageBox.Show("请先设定COM口");
+            fm_set_Com f = new fm_set_Com();
+            f.Show();
+            
+        }
+
+        public fm_receivedata(bool set_com, SerialPort comDevice)
+        {
+            InitializeComponent();
+            this.set_com = set_com;
+            if (set_com)
+            {
+                this.comDevice = comDevice;
+                //将com口的datarece事件绑定到Com_DataReceived函数中
+                comDevice.DataReceived += new SerialDataReceivedEventHandler(Com_DataReceived);
+            }
+            else
+            {
+
+            }
+        }
+        private void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] ReDatas = new byte[comDevice.BytesToRead];
+            comDevice.Read(ReDatas, 0, ReDatas.Length);
+            this.AddData(ReDatas);
+
+        }
+
+        public void AddData(byte[] data)
+        {
+            String sdata = new ASCIIEncoding().GetString(data);
+            //tbx_receivedata.AppendText(sdata);
+            this.BeginInvoke(new MethodInvoker(delegate
+            {
+
+                tbx_receivedata.AppendText("\r\n");
+
+                tbx_receivedata.AppendText(sdata);
+            }));
+        }
+    }
+}
