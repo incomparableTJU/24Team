ea504c2db6345444193bb6a1d3d726926bfb38b9
miaoxu9999 <2229009552@qq.com>
2017-10-26 22:39:13 +0800

    achieve the function of curve graph

--- Cantool/Cantool/curveForm.Designer.cs
+++ Cantool/Cantool/curveForm.Designer.cs
@@ -33,16 +33,17 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnStop = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbx_message = new System.Windows.Forms.ComboBox();
+            this.cbx_signal = new System.Windows.Forms.ComboBox();
+            this.cbx_count = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(350, 12);
-            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Location = new System.Drawing.Point(417, 8);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(100, 29);
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "开始";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -51,10 +52,9 @@
             // chart1
             // 
             this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.chart1.Location = new System.Drawing.Point(5, 52);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Location = new System.Drawing.Point(4, 42);
             this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(650, 400);
+            this.chart1.Size = new System.Drawing.Size(488, 320);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
             // 
@@ -65,35 +65,57 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(480, 12);
-            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStop.Location = new System.Drawing.Point(507, 8);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(100, 29);
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
             this.btnStop.TabIndex = 2;
             this.btnStop.Text = "停止";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
-            // comboBox1
+            // cbx_message
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(44, 13);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
-            this.comboBox1.TabIndex = 3;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cbx_message.FormattingEnabled = true;
+            this.cbx_message.Location = new System.Drawing.Point(12, 13);
+            this.cbx_message.Name = "cbx_message";
+            this.cbx_message.Size = new System.Drawing.Size(121, 20);
+            this.cbx_message.TabIndex = 3;
+            this.cbx_message.SelectedIndexChanged += new System.EventHandler(this.cbx_message_SelectedIndexChanged);
             // 
-            // curveForm
+            // cbx_signal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.cbx_signal.FormattingEnabled = true;
+            this.cbx_signal.Location = new System.Drawing.Point(139, 12);
+            this.cbx_signal.Name = "cbx_signal";
+            this.cbx_signal.Size = new System.Drawing.Size(121, 20);
+            this.cbx_signal.TabIndex = 4;
+            this.cbx_signal.SelectedIndexChanged += new System.EventHandler(this.cbx_signal_SelectedIndexChanged);
+            // 
+            // cbx_count
+            // 
+            this.cbx_count.FormattingEnabled = true;
+            this.cbx_count.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "20"});
+            this.cbx_count.Location = new System.Drawing.Point(266, 11);
+            this.cbx_count.Name = "cbx_count";
+            this.cbx_count.Size = new System.Drawing.Size(121, 20);
+            this.cbx_count.TabIndex = 5;
+            this.cbx_count.SelectedIndexChanged += new System.EventHandler(this.cbx_count_SelectedIndexChanged);
+            // 
+            // CurveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1211, 595);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(908, 476);
+            this.Controls.Add(this.cbx_count);
+            this.Controls.Add(this.cbx_signal);
+            this.Controls.Add(this.cbx_message);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.btnStart);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "curveForm";
+            this.Name = "CurveForm";
             this.Text = "curvegraph";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
@@ -104,6 +126,8 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbx_message;
+        private System.Windows.Forms.ComboBox cbx_signal;
+        private System.Windows.Forms.ComboBox cbx_count;
     }
 }
--- Cantool/Cantool/curveForm.cs
+++ Cantool/Cantool/curveForm.cs
@@ -1,9 +1,12 @@
 ﻿
+using Cantool.DAL;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,22 +18,66 @@ namespace Cantool
 {
     public partial class CurveForm : Form
     {
-        private Queue<double> dataQueue = new Queue<double>(100);    
-        private int num = 10;//每次删除增加几个点
+        private Queue<double> dataQueue = new Queue<double>();
+        private int num = 1;//每次删除增加几个点
         List<float> val = new List<float>();
         List<string> signalname = new List<string>();
         messagesignal sig = new messagesignal();
         string current;//当前选择的名字
-        public  CurveForm()
+        int count = 0;
+
+
+        /// <summary>
+        /// 绑定message数据
+        /// </summary>
+        void bind_cbx_message()
+        {
+            List<Cantool.Message> database = Message_DataBase.get_all_Message();
+            if (database != null && database.Count() > 0)
+            {
+                this.cbx_message.DataSource = database;//绑定数据
+                this.cbx_message.ValueMember = "messageId";
+                this.cbx_message.DisplayMember = "messageName";
+            }
+            else
+            {
+                MessageBox.Show("请先载入数据");
+            }
+        }
+        /// <summary>
+        /// 清理函数，设定初始值
+        /// </summary>
+        private void clear()
+        {
+            count = 0;
+            if(this.chart1.Series.Count > 0)
+            this.chart1.Series[0].Points.Clear();
+            dataQueue.Clear();
+        }
+        public CurveForm()
         {
             InitializeComponent();
-            
-            val.Add(34);
-            val.Add(50);
-            string[] name = { "si1", "si2", "si3" };
-            float[] value = { 23, 45, 67 };
-            
-            sig.initsignal(3, name, value, comboBox1);
+            bind_cbx_message();
+            InitChart();
+
+            this.chart1.Series[0].Points.Clear();
+            this.cbx_count.SelectedIndex = 0;
+            Com.get_com().DataReceived += new SerialDataReceivedEventHandler(Com_DataReceived);
+
+            //val.Add(3);
+            //val.Add(0);
+            //val.Add(7);
+            //val.Add(50);
+            //val.Add(50);
+            //val.Add(50);
+            //val.Add(50);
+            //val.Add(50);
+            //val.Add(50);
+            //val.Add(50);
+            //string[] name = { "si1", "si2", "si3" };
+            //float[] value = { 23, 45, 67 };
+
+            // sig.initsignal(3, name, value, comboBox1);
         }
         /// <summary>
 
@@ -44,8 +91,14 @@ namespace Cantool
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            this.timer1.Start();
+            //this.timer1.Start();
             InitChart();
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+            }
         }
 
         private void btnStop_Click(object sender, EventArgs e)
@@ -98,9 +151,9 @@ namespace Cantool
             this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
 
             this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
-        //    chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
-         //   chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = false;
-         //   chart1.ChartAreas[0].AxisX.ScaleView.Size = 20;
+            //    chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            //   chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = false;
+            //   chart1.ChartAreas[0].AxisX.ScaleView.Size = 20;
 
             //设置标题        
             this.chart1.Titles.Add("S01");
@@ -115,8 +168,30 @@ namespace Cantool
 
             this.chart1.Series[0].Color = Color.Red;
             //线的类型
-            this.chart1.Series[0].ChartType = SeriesChartType.Spline;       
+            this.chart1.Series[0].ChartType = SeriesChartType.Spline;
             this.chart1.Series[0].Points.Clear();
+
+
+            chart1.Series[0].BorderWidth = 2;
+            //标记点边框颜色      
+            chart1.Series[0].MarkerBorderColor = Color.Blue;
+            //标记点边框大小
+            chart1.Series[0].MarkerBorderWidth = 3; //chart1.;// Xaxis 
+            //标记点中心颜色
+            chart1.Series[0].MarkerColor = Color.White;//AxisColor
+            //标记点大小
+            chart1.Series[0].MarkerSize = 8;
+            //标记点类型     
+            chart1.Series[0].MarkerStyle = MarkerStyle.Circle;
+
+            //设置数值显示
+            chart1.Series[0].IsValueShownAsLabel = true;
+            //将文字移到外侧
+            chart1.Series[0]["PieLabelStyle"] = "Outside";
+            //绘制黑色的连线
+            chart1.Series[0]["PieLineColor"] = "Black";
+
+
 
         }
         //更新队列中的值
@@ -132,25 +207,176 @@ namespace Cantool
             }
             for (int i = 0; i < num; i++)
             {
-              
-                foreach (int elements in val)     
-                dataQueue.Enqueue(elements);
-               }          
-        }
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            for(int i=0;i<sig.sigcount();i++)
-            {
-                if (comboBox1.SelectedIndex == i)
-                {
-                    
-                    current = comboBox1.Text.ToString();
-                    MessageBox.Show("显示的是" + current);
-                }
+
+                foreach (int elements in val)
+                    dataQueue.Enqueue(elements);
             }
-              
+        }
+        //private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    for(int i=0;i<sig.sigcount();i++)
+        //    {
+        //        if (comboBox1.SelectedIndex == i)
+        //        {
+
+        //            current = comboBox1.Text.ToString();
+        //            MessageBox.Show("显示的是" + current);
+        //        }
+        //    }
+
+        //}
+
+        private void cbx_message_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //get displayed message
+            Cantool.Message selectedMessage = (Cantool.Message)this.cbx_message.SelectedItem;
+            MessageBox.Show(selectedMessage.messageId.ToString());
+            List<Signal> toBeDisplaySG = new List<Signal>();
+            List<string> signals = selectedMessage.signals;
+            for (int i = 0; i < signals.Count(); i++)
+            {
+                Signal temp = new Signal();
+                Signal get = Signal.getSignal(signals[i]);
+                temp.signal = get.signal;
+                temp.signalName = get.signalName;
+                temp.startBit = get.startBit;
+                temp.bitLength = get.bitLength;
+                temp.bitStyle = get.bitLength;
+                temp.A = get.A;
+                temp.B = get.B;
+                temp.C = get.C;
+                temp.D = get.D;
+                temp.physicalUnit = get.physicalUnit;
+                temp.nodeNames = get.nodeNames;
+                toBeDisplaySG.Add(temp);
+            }
+
+            this.cbx_signal.DataSource = toBeDisplaySG;
+            this.cbx_signal.ValueMember = "signalName";
         }
 
-        
+
+        private DataTable get_signal_value(String signalName, int count)
+        {
+            //数据库字符串，暂时放到这里
+            DbManager.Ins.ConnStr = "server = localhost; user id = root; password = root; database = cantool";
+            //String sql = "select can_message.signal_value from can_message where can_message.signal_name="mx"  ORDER BY can_message.time desc limit 1";
+            String sql = "select can_message.signal_value from can_message where can_message.signal_name=?signal_name  ORDER BY can_message.time desc limit 0,?count";
+            List<MySqlParameter> Paramter = new List<MySqlParameter>();
+            Paramter.Add(new MySqlParameter("?signal_name", signalName));
+            Paramter.Add(new MySqlParameter("?count", count));
+            DataTable data = DbManager.Ins.ExcuteDataTable(sql, Paramter.ToArray());
+            return data;
+        }
+        private void cbx_count_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clear();
+            set_curve_value();
+        }
+
+
+        /// <summary>
+        /// 设置曲线的值
+        /// </summary>
+        private void set_curve_value()
+        {
+            int count = Convert.ToInt32(this.cbx_count.SelectedItem);
+            Signal selected_signal = (Signal)this.cbx_signal.SelectedItem;
+            String signal_name = selected_signal.signalName;
+            DataTable dt = get_signal_value(signal_name, count);
+            if (dt == null)
+            {
+                MessageBox.Show("当前不存在所选signal的数据");
+                return;
+            }
+            else
+            {
+                count = 0;
+                for (count = 0; count < dt.Rows.Count; count++)
+                {
+                    //
+                    String str = dt.Rows[count]["signal_value"].ToString();
+                    dataQueue.Enqueue(Convert.ToDouble(str));
+                    this.chart1.Series[0].Points.AddXY((count + 1), Convert.ToDouble(str));
+                    
+                }
+
+            }
+        }
+
+        private void cbx_signal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clear();
+            set_curve_value();
+        }
+
+
+        private void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort comDevice = Com.get_com();
+            if (comDevice != null)
+            {
+                byte[] ReDatas = new byte[comDevice.BytesToRead];
+                comDevice.Read(ReDatas, 0, ReDatas.Length);
+
+                String sdata = new ASCIIEncoding().GetString(ReDatas);
+                if (sdata == null || sdata.Trim() == "")
+                {
+                    String current_data = Message_DataBase.get_current_data();
+                    if (current_data != null)
+                    {
+                        sdata = current_data;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                Message_DataBase.set_current_data(sdata);
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                Calculate cal = new Calculate();
+                dic = cal.Decode(sdata);
+
+
+                if (this.IsHandleCreated)
+                {
+                    this.BeginInvoke(new MethodInvoker(delegate
+                    {
+                        //for (int i = 0; i < 10; i++)
+                        //{
+
+                        //}
+                        //aGauge2.Value = count;
+                        //count = count + 10;
+                        Signal selected_signal = (Signal)this.cbx_signal.SelectedItem;
+                        foreach (KeyValuePair<string, string> kv in dic)
+                        {
+                            if (kv.Key == selected_signal.signalName)
+                            {
+                                if (this.chart1.Series.Count > 0)
+                                {
+                                    this.chart1.Series[0].Points.Clear();  
+                                }
+                                dataQueue.Enqueue(Convert.ToDouble(kv.Value));
+                                for (int i = 0; i < dataQueue.Count; i++)
+                                {
+                                    this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+                                }
+
+
+                                break;
+                            }
+
+                        }
+                    }));
+                }
+            }
+            else
+            {
+                MessageBox.Show("请设置COM口");
+            }
+
+        }
     }
 }
+
