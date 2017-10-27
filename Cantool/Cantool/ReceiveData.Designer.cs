fdff6d5517d66bc8ccf12a3bac8e544d9189dfc3
wangqiang <838660384@qq.com>
2017-10-27 21:15:00 +0800

    修改接收显示

--- Cantool/Cantool/ReceiveData.Designer.cs
+++ Cantool/Cantool/ReceiveData.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Set_Com
+﻿using AdvancedDataGridView;
+
+namespace Set_Com
 {
     partial class fm_receivedata
     {
@@ -28,34 +30,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbx_receivedata = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.imageStrip = new System.Windows.Forms.ImageList(this.components);
+            this.treeGridView1 = new AdvancedDataGridView.TreeGridView();
+            this.attachmentColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.subjectColumn = new AdvancedDataGridView.TreeGridColumn();
+            this.fromColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+           // this.dateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new AdvancedDataGridView.TreeGridColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.treeGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tbx_receivedata
+            // imageStrip
             // 
-            this.tbx_receivedata.Location = new System.Drawing.Point(20, 28);
-            this.tbx_receivedata.Multiline = true;
-            this.tbx_receivedata.Name = "tbx_receivedata";
-            this.tbx_receivedata.Size = new System.Drawing.Size(381, 256);
-            this.tbx_receivedata.TabIndex = 0;
+            this.imageStrip.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageStrip.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageStrip.TransparentColor = System.Drawing.Color.Transparent;
             // 
-            // fm_receivedata
+            // treeGridView1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 352);
-            this.Controls.Add(this.tbx_receivedata);
-            this.Name = "fm_receivedata";
-            this.Text = "接收数据";
+            this.treeGridView1.AllowUserToAddRows = false;
+            this.treeGridView1.AllowUserToDeleteRows = false;
+            this.treeGridView1.AllowUserToOrderColumns = true;
+            this.treeGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.treeGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.treeGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.treeGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.attachmentColumn,
+            this.subjectColumn,
+            this.fromColumn});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.treeGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.treeGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.treeGridView1.ImageList = null;
+            this.treeGridView1.Location = new System.Drawing.Point(12, 12);
+            this.treeGridView1.Name = "treeGridView1";
+            this.treeGridView1.RowHeadersVisible = false;
+            this.treeGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.treeGridView1.ShowLines = false;
+            this.treeGridView1.Size = new System.Drawing.Size(530, 442);
+            this.treeGridView1.TabIndex = 3;
+            this.treeGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.treeGridView1_CellContentClick);
+            // 
+            // attachmentColumn
+            // 
+            this.attachmentColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.NullValue = null;
+            this.attachmentColumn.DefaultCellStyle = dataGridViewCellStyle1;
+            this.attachmentColumn.FillWeight = 51.53443F;
+            this.attachmentColumn.HeaderText = "";
+            this.attachmentColumn.MinimumWidth = 25;
+            this.attachmentColumn.Name = "attachmentColumn";
+            this.attachmentColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.attachmentColumn.Width = 25;
+            // 
+            // subjectColumn
+            // 
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.subjectColumn.DefaultCellStyle = dataGridViewCellStyle2;
+            this.subjectColumn.DefaultNodeImage = null;
+            this.subjectColumn.FillWeight = 350.9562F;
+            this.subjectColumn.HeaderText = "Signalname";
+            this.subjectColumn.Name = "subjectColumn";
+            this.subjectColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // fromColumn
+            // 
+            this.fromColumn.FillWeight = 100F;
+            this.fromColumn.HeaderText = "Value";
+            this.fromColumn.Name = "fromColumn";
+            this.fromColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dateColumn
+            // 
+           // this.dateColumn.FillWeight = 100F;
+          //  this.dateColumn.HeaderText = "unit";
+          //  this.dateColumn.Name = "dateColumn";
+          //  this.dateColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column1
+            // 
+            this.Column1.DefaultNodeImage = null;
+            this.Column1.Name = "Column1";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(554, 466);
+            this.Controls.Add(this.treeGridView1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(9);
+            this.Text = "接收到的物理值";
+            this.Load += new System.EventHandler(this.fm_receivedata_Load);
+            this.Shown += new System.EventHandler(this.fm_receivedata_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.treeGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
-            //this.Load += new System.EventHandler(this.fm_receivedata_Load);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbx_receivedata;
-        
+        private TreeGridView expandGrid1;
+        private TreeGridNode treeGridNode1;
+        private TreeGridNode treeGridNode2;
+        private TreeGridNode treeGridNode3;
+        private TreeGridView treeGridView1;
+        private TreeGridColumn treeGridColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private TreeGridColumn treeGridColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private TreeGridColumn Column1;
+        private System.Windows.Forms.ImageList imageStrip;
+        private System.Windows.Forms.DataGridViewImageColumn attachmentColumn;
+        private TreeGridColumn subjectColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fromColumn;
+      //  private System.Windows.Forms.DataGridViewTextBoxColumn dateColumn;
+
     }
 }
--- Cantool/Cantool/ReceiveData.cs
+++ Cantool/Cantool/ReceiveData.cs
@@ -1,4 +1,5 @@
-﻿using Cantool;
+﻿using AdvancedDataGridView;
+using Cantool;
 using Cantool.DAL;
 using MySql.Data.MySqlClient;
 using System;
@@ -16,7 +17,8 @@ namespace Set_Com
 {
     public partial class fm_receivedata : Form
     {
-        
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        List<string> signalname;
         /// <summary>
         /// Com口是否设定
         /// </summary>
@@ -44,6 +46,7 @@ namespace Set_Com
                 this.comDevice = Com.get_com();
                 //将com口的datarece事件绑定到Com_DataReceived函数中
                 comDevice.DataReceived += new SerialDataReceivedEventHandler(Com_DataReceived);
+               
             }
             else
             {
@@ -66,10 +69,10 @@ namespace Set_Com
                 return;
             }
             Message_DataBase.set_current_data(sdata);
-            Dictionary<string, string> dic = new Dictionary<string, string>();
-            Calculate cal = new Calculate();
-            this.AddData(ReDatas);
+           
+            Calculate cal = new Calculate();          
             dic = cal.Decode(sdata);
+           
             //dic长度为0
             //MessageBox.Show("dic的长度为：" + dic.Count().ToString());
             
@@ -87,44 +90,94 @@ namespace Set_Com
                 Paramter.Add(new MySqlParameter("?time", DateTime.Now.ToLocalTime().ToString()));
                 int insert = DbManager.Ins.ExecuteNonquery(sql, Paramter.ToArray());
                 byte[] byteArray = System.Text.Encoding.ASCII.GetBytes(temp);
-                this.AddData(byteArray);
-            }
+                // this.AddData(byteArray);
+               }
         }
+        private void fm_receivedata_Load(object sender, System.EventArgs e)
+       {
+            attachmentColumn.DefaultCellStyle.NullValue = null;
 
+            // load image strip
+            this.imageStrip.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageStrip.TransparentColor = System.Drawing.Color.Magenta;
+            this.imageStrip.ImageSize = new Size(16, 16);
+            //   this.imageStrip.Images.AddStrip(Cantool.Properties.Resources.newGroupPostIconStrip);
 
-        public void AddData(byte[] data)
+            treeGridView1.ImageList = imageStrip;
+
+            // attachment header cell
+           // this.attachmentColumn.HeaderCell = new AttachmentColumnHeader(imageStrip.Images[2]);
+ 
+         }
+        
+        private void fm_receivedata_Shown(object sender, EventArgs e)
         {
-            String sdata = new ASCIIEncoding().GetString(data);
-            //tbx_receivedata.AppendText(sdata);
-            if (this.IsHandleCreated)
+            Font boldFont = new Font(treeGridView1.DefaultCellStyle.Font, FontStyle.Bold);
+            List<TreeGridNode> node = new List<TreeGridNode>();
+            int i = 0;
+            foreach (KeyValuePair<string, string> kv in dic)
             {
-                this.BeginInvoke(new MethodInvoker(delegate
+                if (!signalname.Contains(kv.Key))
                 {
-
-                    tbx_receivedata.AppendText("\r\n");
-                    //tbx_receivedata.Multiline = Enabled;
-                    tbx_receivedata.AppendText(sdata);
-                }));
-            }
+                    signalname.Add(kv.Key);
+                    
+                    node[i] = treeGridView1.Nodes.Add(null, kv.Key, kv.Value);
+                    node[i].ImageIndex = 0;
+                    node[i].DefaultCellStyle.Font = boldFont;
+                    i++;
+                                 
+                }
+                else
+                {
+                    for (int j= 0; j < signalname.Count(); j++)
+                    {
+                        if (kv.Key ==signalname[j])
+                        {
+                            node[j].Nodes.Add(null," ",kv.Value);
+                            node[j].ImageIndex = 0;
+                            node[j].DefaultCellStyle.Font = boldFont;
+                        }
+                }
+                }
+                   
+               
                 
+
+                //  node = node.Nodes.Add(null, "Re: Using DataView filter when binding to DataGridView", "tab", @"10/19/2005 1:02 AM");
+                //  node.ImageIndex = 1;
+            }
+            TreeGridNode node2 = treeGridView1.Nodes.Add(null, "kv.Key"," kv.Value");
+            node2.ImageIndex = 0;
+            node2.DefaultCellStyle.Font = boldFont;
+            node2 = node2.Nodes.Add(null, "Re: Using DataView filter when binding to DataGridView", "tab");
+            node2.ImageIndex = 1;
         }
 
-        //private void InitializeComponent()
-        //{
-        //    this.SuspendLayout();
-        //    // 
-        //    // fm_receivedata
-        //    // 
-        //    this.ClientSize = new System.Drawing.Size(284, 262);
-        //    this.Name = "fm_receivedata";
-            
-        //    this.ResumeLayout(false);
 
-        //}
+        internal class AttachmentColumnHeader : DataGridViewColumnHeaderCell
+        {
+            public Image _image;
+            public AttachmentColumnHeader(Image img)
+                : base()
+            {
+                this._image = img;
+            }
+            protected override void Paint(Graphics graphics, Rectangle clipBounds, Rectangle cellBounds, int rowIndex, DataGridViewElementStates dataGridViewElementState, object value, object formattedValue, string errorText, DataGridViewCellStyle cellStyle, DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts)
+            {
+                base.Paint(graphics, clipBounds, cellBounds, rowIndex, dataGridViewElementState, value, formattedValue, errorText, cellStyle, advancedBorderStyle, paintParts);
+                graphics.DrawImage(_image, cellBounds.X + 4, cellBounds.Y + 2);
+            }
+            protected override object GetValue(int rowIndex)
+            {
+                return null;
+            }
+        }
 
-        //private void fm_receivedata_Load(object sender, EventArgs e)
-        //{
-        //    this.Dispose(false);
-        //}
+        private void treeGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
     }
 }
